17f9186dc3e2fe88247086636e8f02d03446d4c0
Ade Koswara <kuncen.backup@gmail.com>
2026-05-18 13:18:55 +0700

    Add files via upload

---
+++ http-auth.php
@@ -0,0 +1,24 @@
+                                                                     <?php
+
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+$app      = $_POST['app'] ?? '';
+$token    = $_POST['token'] ?? '';
+
+$valid_user = "ade";
+$valid_pass = "123";
+$valid_token = "abc123";
+
+if ($username === $valid_user &&
+    $password === $valid_pass &&
+    $app === "portal" &&
+    $token === $valid_token) {
+
+    http_response_code(200);
+    echo "LOGIN SUCCESS";
+
+} else {
+    http_response_code(401);
+    echo "LOGIN FAILED";
+}
+?>
